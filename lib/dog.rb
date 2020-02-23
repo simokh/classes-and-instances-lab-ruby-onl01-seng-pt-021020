d162f8f22f4116d478db8dcc6542fc22fd55c89a
Simo Khouili <m.simokhouili@gmail.com>
2020-02-23 19:09:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,7 @@
 class dog
-fido = puppy
 end
 
-# class dog
-# fido = puppy
-# end
 
-# class dog
-# fido = puppy
-# end
-
-
-# snoopy = puppy3
-# lassie = puppy2
+fido = dog.new
+snoopy = dog.new
+lassie = dog.new
